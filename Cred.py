1114ebe4423e48aeb7509782e5059481d6d8461f
Viral parmar <31970912+vral-parmar@users.noreply.github.com>
2021-07-06 18:55:07 +0530

    Create Cred.py

---
+++ Cred.py
@@ -0,0 +1,11 @@
+# cred.py file
+#Author  : 	Viral Parmar   
+#Github  : 	https://github.com/vral-parmar
+#Twitter : 	https://twitter.com/vral_parmar
+#live Bot:	https://twitter.com/TaelynCorb
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+#enter your API Credential on variable
